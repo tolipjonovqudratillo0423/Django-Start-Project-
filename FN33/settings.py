97ccff3a219c6bcc390298720a29c91b2bccc474
tolipjonovqudratillo0423 <tolipjonovqudratillo0423@gmail.com>
2025-10-16 21:46:10 +0500

    Writing First Django Micro project 0.0.0.2

--- FN33/settings.py
+++ FN33/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main']   
+    'core',]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
