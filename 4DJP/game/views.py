5bbc4766e0924b6e0a037142e2bb8c63971393e9
tolipjonovqudratillo0423 <tolipjonovqudratillo0423@gmail.com>
2025-10-17 15:15:42 +0500

    Writing First Django Micro Project 0.0.0.5 with add project

---
+++ 4DJP/game/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+
+def index(request):
+    posts = Post.objects.all().order_by('-created_at'),
+    return render(request,'core/index.html',{'posts':posts})
