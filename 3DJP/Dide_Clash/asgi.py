0061364066c8b09f9c2e58e324382c2ad29c2693
tolipjonovqudratillo0423 <tolipjonovqudratillo0423@gmail.com>
2025-10-17 14:43:11 +0500

    Writing First Django Micro Project 0.0.0.4 with add project

---
+++ 3DJP/Dide_Clash/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Dide_Clash project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dide_Clash.settings')
+
+application = get_asgi_application()
